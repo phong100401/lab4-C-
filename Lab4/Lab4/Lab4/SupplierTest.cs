e19c5f0f542981e9b6aa33477dc2771ce3cbb201
phong100401 <65002403+phong100401@users.noreply.github.com>
2021-01-05 13:28:50 +0700

    Lab4-C#
    Lab4-C#

---
+++ Lab4/Lab4/Lab4/SupplierTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class SupplierTest
+    {
+        static void Main1(string[] args)
+        {
+            Supplier objSupplier = new Supplier();
+            objSupplier.AcceptDetails();
+            objSupplier.DisplayDetails();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab4/Lab4/Lab4/TestASM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class TestASM
+    {
+       static void Main(string[] args)
+        {
+            Medicine objMedicne = new Medicine();
+            Sale objSale = new Sale();
+            objMedicne.Accept();
+            objSale.Accpect();
+            Console.WriteLine();
+            objMedicne.Print();
+            objSale.Display();
+            Console.ReadKey();
+
+        }
+    }
+}
